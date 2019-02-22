991a810b519bf3f8d5f9f6a0b1d7761dc8e5cdd9
Charlie Hilton <charllie.hiltons@gmail.com>
2019-02-21 21:52:37 -0600

    updating files

---
+++ public/assets/js/view.js
@@ -0,0 +1,136 @@
+
+$(document).ready(function () {
+    //Input field reference where user adds a new joke.
+    var $newItemInput = $("textarea");
+    //console.log($newItemInput)
+    //Display jokes added in the joke list
+    var $jokelistContainer = $(".jokelist-container");
+    //Event listeners for CRUD
+    $(document).on("click", "button.delete", deleteJoke);
+    $(document).on("click", "#jokelist-item", editJoke);
+    $(document).on("keyup", "#jokelist-item", finishEdit);
+    $(document).on("blur", "#jokelist-item", cancelEdit);
+    $(document).on("click", "#jokelist-form", insertJoke);
+
+    //Jokes array
+
+    var jokelist = [];
+  
+    // Getting jokes from database when page loads
+    getJokeList();
+  
+    // This function resets the todos displayed with new todos from the database
+    function initializeRows() {
+      $jokelistContainer.empty();
+      var rowsToAdd = [];
+      for (var i = 0; i < jokelist.length; i++) {
+        rowsToAdd.push(createNewRow(jokelist[i]));
+      }
+      $jokelistContainer.prepend(rowsToAdd);
+    }
+  
+    // This function grabs jokes from the database and updates the view
+    function getJokeList() {
+      $.get("/api/jokeList", function(data) {
+        jokelist = data;
+        initializeRows();
+      });
+    }
+  
+    // This function deletes a todo when the user clicks the delete button
+    function deleteJoke(event) {
+        //event.stopPropogation();
+        var id = $(this).data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/api/jokelist/" + id
+        }).then(getJokeList);
+
+    }
+  
+    // This function handles showing the input box for a user to edit a todo
+    function editJoke() {
+      var currentJoke = $(this).data("joke-list");
+      $(this).children().hide();
+      $(this).children("input.input").val(currentJoke.text);
+      $(this).children("input.input").show();
+      $(this).children("input.input").focus();
+    }
+  
+    // Toggles complete status
+    function toggleComplete(event) {
+      event.stopPropagation();
+      var jokelist = $(this).parent().data("joke-list");
+      jokelist.complete = !jokelist.complete;
+      updateJoke(jokelist);
+    }
+  
+    // This function starts updating a todo in the database if a user hits the "Enter Key"
+    // While in edit mode
+    function finishEdit(event) {
+      var updatedJoke = $(this).data("joke-list");
+      if (event.which === 13) {
+        updatedJoke.text = $(this).children("input").val().trim();
+        $(this).blur();
+        updateJoke(updatedJoke);
+      }
+    }
+  
+    // This function updates a todo in our database
+    function updateJoke(jokelist) {
+      $.ajax({
+        method: "PUT",
+        url: "/api/jokeList",
+        data: jokelist
+      }).then(getJokeList);
+    }
+  
+    // This function is called whenever a todo item is in edit mode and loses focus
+    // This cancels any edits being made
+    function cancelEdit() {
+      var currentJoke = $(this).data("joke-list");
+      if (currentJoke) {
+        $(this).children().hide();
+        $(this).children("input.input").val(currentJoke.text);
+        $(this).children("span").show();
+        $(this).children("button").show();
+      }
+    }
+  
+    // This function constructs a todo-item row
+    function createNewRow(jokelist) {
+        var $newInputRow = $(
+            [
+                "<li class='list-group-item joke-item'>",
+                "<span>",
+                "'" +
+                jokelist.joke + "'",
+                "</span>",
+                "<input type='text' class='edit' style='display: none;'>",
+                " <button class='delete'>delete</button>",
+                "</li>"
+            ].join("")
+        );
+
+        $newInputRow.find("button.delete").data("id", jokelist.id);
+        $newInputRow.find("input.edit").css("display", "none");
+        $newInputRow.data("jokeitem", jokelist);
+        if (jokelist.complete) {
+            $newInputRow.find("span").css("text-decoration", "line-through");
+        }
+        return $newInputRow;
+    }
+    console.log(createNewRow)
+  
+    // This function inserts a new todo into our database and then updates the view
+    function insertJoke(event) {
+        event.preventDefault();
+        var jokelist = {
+            joke: $newItemInput.val().trim()
+        };
+
+        $.post("/api/jokelist", jokelist, getJokeList);
+        $newItemInput.val("");
+    }
+  });
+  
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var bodyParser = require ("body-parser");
+var session = require ("express-session");
+var passport = require ("./config/passport");
+var PORT = process.env.PORT || "development";
+
+// Requiring our models for syncing
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
